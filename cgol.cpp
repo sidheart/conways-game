fb66916263c5064404ff2820a2272c457a6b47fd
Sid Narayan <sidsdn@gmail.com>
2016-12-06 01:04:14 -0800

    Created a working initial game board using NCurses

--- cgol.cpp
+++ cgol.cpp
@@ -6,37 +6,75 @@
 
 using namespace std;
 
-Cgol::Cgol(int nrows, int ncols) {
+Cgol::Cgol(size_t nrows, size_t ncols) 
+	: nrows{nrows}, ncols{ncols}
+{
 	string s;
-	for(int i = 0; i < (2 * ncols) - 1; i++) {
-		if(i % 2 == 0) {
-			s += '*';
-		} else {
-			s += '|';
-		}
+	for(size_t i = 0; i < ncols; i++) {
+		s += ' ';
 	}
-	for(int i = 0; i < nrows; i++) {
+	for(size_t i = 0; i < nrows; i++) {
 		board.push_back(s);
 	}
 }
 
-void Cgol::update(vector<string> board)
+void Cgol::update(vector<string>& board)
 {
 }
 
 void Cgol::init()
 {
-	initscr();			/* Start curses mode 		  */
+	initscr();			
 	cbreak();
 	noecho();
 	keypad(stdscr, TRUE);
-	printw("Hello World !!!");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
-	endwin();			/* End curses mode		  */
+	border(0, 0, 0, 0, 0, 0, 0, 0); // Draw border around screen
+	refresh();
+}
+
+void Cgol::drawBoard(vector<string> board)
+{
+	size_t window_h, window_w;
+	getmaxyx(stdscr, window_h, window_w);
+	size_t x_initial{(window_w / 2) - (ncols)};
+	size_t y_initial{(window_h / 2) - (nrows / 2)};
+
+	/* Draw top border of game board */
+	move(y_initial - 1, x_initial);
+	addch(ACS_ULCORNER);
+	for(size_t j = 0; j < ncols * 2 - 1; j++) {
+			addch(ACS_HLINE);
+	}
+	addch(ACS_URCORNER);
+
+	/* Draw interior contents of game board */
+	for(size_t i = 0; i < nrows; i++) {
+		move(y_initial + i, x_initial);
+		addch(ACS_VLINE);
+		for(size_t j = 0; j < ncols; j++) {
+				if(board[i][j] == ' ')
+					addch(ACS_BULLET);
+				else
+					addch(ACS_DIAMOND);
+				addch(ACS_VLINE);
+		}
+	}
+
+	/* Draw bottom border of game board */
+	move(y_initial + nrows, x_initial);
+	addch(ACS_LLCORNER);
+	for(size_t j = 0; j < ncols * 2 - 1; j++) {
+			addch(ACS_HLINE);
+	}
+	addch(ACS_LRCORNER);
+	refresh();
 }
 
 void Cgol::simulate()
 {
 	init();
+	drawBoard(getBoard());
+	//getUserInput();
+	getch();
+	endwin();	
 }
--- cgol.h
+++ cgol.h
@@ -1,14 +1,18 @@
 #include <vector>
 #include <string>
+#include <ncurses.h>
 
 class Cgol 
 {
 	public:
-		Cgol(int nrows, int ncols);
-		void simulate();
+		Cgol(size_t nrows, size_t ncols);
 		void init();
-		void update(std::vector<std::string> board);
+		void update(std::vector<std::string>& board);
+		void drawBoard(std::vector<std::string> board);
+		void simulate();
 		std::vector<std::string> getBoard() { return board; }
 	private:
+		WINDOW* win;
 		std::vector<std::string> board;	
+		size_t nrows, ncols;
 };
